129e22e311df579b2e03235cdc791f24b5430305
Crowdbotics <team@crowdbotics.com>
2020-05-27 20:15:35 +0000

    Update models

--- gobusiness_17467/wsgi.py
+++ gobusiness_17467/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gobusiness_17467.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gobusiness_17467.settings")
 
 application = get_wsgi_application()
